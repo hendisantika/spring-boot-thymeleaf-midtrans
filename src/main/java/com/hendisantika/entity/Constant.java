f70948fa2157bd542cbf898ef49426a7450df616
hendisantika <hendisantika@yahoo.co.id>
2025-03-09 06:40:40 +0700

    feat: Add Constant🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/entity/Constant.java
@@ -0,0 +1,23 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-thymeleaf-midtrans
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/10/23
+ * Time: 11:11
+ * To change this template use File | Settings | File Templates.
+ */
+public final class Constant {
+    public static final String sandboxServerKey = "YOUR-SANDBOX-SERVER-KEY";
+    public static final String sandboxClientKey = "YOUR_SANDBOX_CLIENT_KEY";
+    public static final String secondServerKey = "YOUR-SECOND-SERVER-KEY";
+    public static final String secondClientKey = "YOUR-SECOND-CLIENT-KEY";
+
+    public static final String sandboxCreatorKey = "YOUR-SANDBOX-CREATOR-KEY";
+    public static final String sandboxApproverKey = "YOUR-SANDBOX-APPROVER-KEY";
+
+    public static boolean isProduction = false;
+}
