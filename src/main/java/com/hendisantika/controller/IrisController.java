18906cccd4594560a4e21bdf0175fc87f2aeb4a2
hendisantika <hendisantika@yahoo.co.id>
2025-03-09 06:57:11 +0700

    feat: Add IrisController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/controller/IrisController.java
@@ -0,0 +1,37 @@
+package com.hendisantika.controller;
+
+import com.midtrans.Config;
+import com.midtrans.ConfigFactory;
+import com.midtrans.service.MidtransIrisApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+import static com.hendisantika.entity.Constant.sandboxCreatorKey;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-thymeleaf-midtrans
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/10/23
+ * Time: 11:10
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@Slf4j
+public class IrisController {
+
+    MidtransIrisApi irisApi = new ConfigFactory(
+            new Config(sandboxCreatorKey,
+                    null,
+                    false))
+            .getIrisApi();
+
+    public IrisController() {
+        String irisSandboxMerchantKey = "YOUR-SANDBOX-MERCHANT-KEY";
+        irisApi.apiConfig().setIRIS_MERCHANT_KEY(irisSandboxMerchantKey);
+    }
+
+
+}
