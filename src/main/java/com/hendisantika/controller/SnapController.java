85a025b5ab81f1f6376d0587bbf5fbbd246a475d
hendisantika <hendisantika@yahoo.co.id>
2025-03-09 07:02:57 +0700

    feat: Add SnapController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/controller/SnapController.java
@@ -0,0 +1,29 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.config.MockupData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-thymeleaf-midtrans
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/9/23
+ * Time: 09:24
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequiredArgsConstructor
+public class SnapController {
+    //Data transaction Mockup
+    private final MockupData dataMockup;
+    @Value("${midtrans.serverkey}")
+    private String sandboxServerKey;
+    @Value("${midtrans.clientkey}")
+    private String sandboxClientKey;
+
+
+}
