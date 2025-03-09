fd314673d12fc682b1f76d18cdc551f23d019cf5
hendisantika <hendisantika@yahoo.co.id>
2025-03-09 07:03:54 +0700

    feat: Add checkout API in SnapController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/controller/SnapController.java
+++ src/main/java/com/hendisantika/controller/SnapController.java
@@ -1,12 +1,20 @@
 package com.hendisantika.controller;
 
 import com.hendisantika.config.MockupData;
+import com.midtrans.Midtrans;
+import com.midtrans.httpclient.SnapApi;
+import com.midtrans.httpclient.error.MidtransError;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -38,5 +46,47 @@ public class SnapController {
         return "snap/snap";
     }
 
+    @PostMapping(value = "/snap/check-out")
+    public String checkout(@RequestParam(value = "enablePay", required = false) List<String> listPay,
+                           @RequestParam(value = "snapType") String snapType,
+                           Model model) throws MidtransError {
 
+        Midtrans.clientKey = sandboxClientKey;
+        Midtrans.serverKey = sandboxServerKey;
+        // Get ClientKey from Midtrans Configuration class
+        String clientKey = Midtrans.getClientKey();
+
+        // New Map Object for JSON raw request body
+        Map<String, Object> requestBody = new HashMap<>();
+
+        // Add enablePayment from @RequestParam to dataMockup
+        List<String> paymentList = new ArrayList<>();
+        if (listPay != null) {
+            paymentList.addAll(listPay);
+        }
+        Map<String, String> creditCard = new HashMap<>();
+        creditCard.put("secure", "true");
+        dataMockup = new MockupData();
+        dataMockup.creditCard(creditCard);
+        dataMockup.enablePayments(paymentList);
+
+        // PutAll data mockUp to requestBody
+        requestBody.putAll(dataMockup.initDataMock());
+
+        // send data to frontEnd snapPopUp
+        if (snapType.equals("snap")) {
+            model.addAttribute("result", requestBody);
+            model.addAttribute("clientKey", clientKey);
+            // token object getData token to API with createTransactionToken() method return String token
+            model.addAttribute("transactionToken", SnapApi.createTransactionToken(requestBody));
+            return "snap/check-out";
+
+            // send data to frontEnd redirect-url
+        } else {
+            model.addAttribute("result", requestBody);
+            // redirectURL get url redirect to API with createTransactionRedirectUrl() method, with return String url redirect
+            model.addAttribute("redirectURL", SnapApi.createTransactionRedirectUrl(requestBody));
+            return "snap/check-out";
+        }
+    }
 }
